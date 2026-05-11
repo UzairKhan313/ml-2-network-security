e083489a25b4272608a36ee831337e3fcd6677a1
UzairKhan313 <uzairkhaan2003@gmail.com>
2026-05-11 09:32:26 +0500

    cloud s3 data storage

--- networksecurity/pipelines/training_pipeline.py
+++ networksecurity/pipelines/training_pipeline.py
@@ -10,6 +10,7 @@ from networksecurity.components.model_trainer import ModelTrainer
 
 from networksecurity.constant.training_pipeline import TRAINING_BUCKET_NAME
 from networksecurity.constant.training_pipeline import SAVED_MODEL_DIR
+from networksecurity.cloud.s3_syncer import S3Sync
 
 from networksecurity.entity.config_entity import (
     TrainingPipelineConfig,
@@ -31,6 +32,7 @@ class TrainingPipeline:
     def __init__(self):
         try:
             self.training_pipeline_config = TrainingPipelineConfig()
+            self.s3_sync = S3Sync()
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
@@ -84,6 +86,28 @@ class TrainingPipeline:
         except Exception as e:
             raise NetworkSecurityException(e, sys)
 
+    ## local artifact is going to s3 bucket
+    def sync_artifact_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/artifact/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(
+                folder=self.training_pipeline_config.artifact_dir,
+                aws_bucket_url=aws_bucket_url,
+            )
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
+    ## local final model is going to s3 bucket
+    def sync_saved_model_dir_to_s3(self):
+        try:
+            aws_bucket_url = f"s3://{TRAINING_BUCKET_NAME}/final_model/{self.training_pipeline_config.timestamp}"
+            self.s3_sync.sync_folder_to_s3(
+                folder=self.training_pipeline_config.model_dir,
+                aws_bucket_url=aws_bucket_url,
+            )
+        except Exception as e:
+            raise NetworkSecurityException(e, sys)
+
     def start_model_trainer(
         self, data_transformation_artifact: DataTransformationArtifact
     ) -> ModelTrainerArtifact:
@@ -117,8 +141,8 @@ class TrainingPipeline:
                 data_transformation_artifact=data_transformation_artifact
             )
 
-            # self.sync_artifact_dir_to_s3()
-            # self.sync_saved_model_dir_to_s3()
+            self.sync_artifact_dir_to_s3()
+            self.sync_saved_model_dir_to_s3()
 
             return model_trainer_artifact
         except Exception as e:
