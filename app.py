15922a044f294f56517e594b535ccc7edbe7e2fd
UzairKhan313 <uzairkhaan2003@gmail.com>
2026-05-11 09:18:55 +0500

    done batch pusher

--- app.py
+++ app.py
@@ -20,6 +20,7 @@ from fastapi import FastAPI, File, UploadFile, Request
 from uvicorn import run as app_run
 from fastapi.responses import Response
 from starlette.responses import RedirectResponse
+from fastapi.templating import Jinja2Templates
 import pandas as pd
 
 from networksecurity.exception.exception import NetworkSecurityException
@@ -28,7 +29,7 @@ from networksecurity.pipelines.training_pipeline import TrainingPipeline
 from networksecurity.utils.main_utils.utils import load_object
 from networksecurity.constant.training_pipeline import DATA_INGESTION_COLLECTION_NAME
 from networksecurity.constant.training_pipeline import DATA_INGESTION_DATABASE_NAME
-from fastapi.templating import Jinja2Templates
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
 
 client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
 database = client[DATA_INGESTION_DATABASE_NAME]
@@ -61,6 +62,31 @@ async def train_route():
         train_pipeline = TrainingPipeline()
         train_pipeline.run_pipeline()
         return Response("Training is successful")
+    except Exception as e:
+        raise NetworkSecurityException(e, sys)
+
+
+@app.post("/predict")
+async def predict_route(request: Request, file: UploadFile = File(...)):
+    try:
+        df = pd.read_csv(file.file)
+        preprocessor = load_object("final_model/preprocessor.pkl")
+        final_model = load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocessor, model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df["predicted_column"] = y_pred
+        print(df["predicted_column"])
+        # df['predicted_column'].replace(-1, 0)
+        # return df.to_json()
+        df.to_csv("prediction_output/output.csv")
+        table_html = df.to_html(classes="table table-striped")
+        # print(table_html)
+        return templates.TemplateResponse(
+            "table.html", {"request": request, "table": table_html}
+        )
+
     except Exception as e:
         raise NetworkSecurityException(e, sys)
 
